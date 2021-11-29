52fb1dd7b56faaf6ace20c9680d31a6a90f1a45c
Ian Nkwocha <11nkwochaian@gmail.com>
2021-11-29 10:22:07 +0000

    Created standard and ranked grade books

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook:StandardGradeBook
+    {
+        public RankedGradeBook(string name):base(name)
+        {
+            this.type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
